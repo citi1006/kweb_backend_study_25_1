f6772b5662e40e5f53399a9a802b8107f2e58ef0
citi1006 <citi1006@naver.com>
2025-05-01 16:20:26 +0900

    exercise2.1

---
+++ cases.js
@@ -0,0 +1,33 @@
+const factorial = n =>{
+    if(n<0)return -1;
+    if(n === 1 || n === 0) return 1;
+    return n*factorial(n-1);
+}
+
+const permutation = (n,r) =>{
+    if(n<r||n<0||r<0) return -1;
+    return factorial(n)/factorial(n-r);
+}
+
+const combination = (n,r) =>{
+    if(n<r||n<0||r<0) return -1;
+    return factorial(n)/(factorial(n-r)*factorial(r));
+}
+
+const multiPermutation = (n,r) =>{
+    if(n < 0||r < 0) return -1;
+    return n**r;
+}
+
+const multiCombination = (n,r) =>{
+    if(n<0||r<0) return -1;
+    if(r == 0) return 1;
+    return combination(n+r-1,r);
+}
+
+module.exports = {
+    permutation,
+    combination,
+    multiCombination,
+    multiPermutation,
+};
